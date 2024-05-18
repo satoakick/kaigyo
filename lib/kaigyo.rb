3dc8e56aa66e4a42c86447122433b057165745db
satoakick <satoakick@gmail.com>
2024-05-18 10:19:48 +0900

    Impl formatting for simple query

--- lib/kaigyo.rb
+++ lib/kaigyo.rb
@@ -2,7 +2,103 @@ require "kaigyo/version"
 
 module Kaigyo
   class Error < StandardError; end
-  def kaigyo ; "kaigyo"; end
+
+  WITH = 'with'
+  SELECT = 'select'
+  FROM = 'from'
+  WHERE = 'where'
+  GROUP = 'group'
+  HAVING = 'having'
+  WINDOW = 'window'
+  UNION = 'union'
+  INTERSECT = 'intersect'
+  EXCEPT = 'except'
+  ORDER = 'order'
+  LIMIT = 'limit'
+  OFFSET = 'offset'
+  FETCH = 'fetch'
+  FOR = 'for'
+  BY = 'by'
+  LEFT_PAREN = '('
+  RIGHT_PAREN = ')'
+
+  class Tokenizer
+    attr_reader :source, :current
+
+    def initialize(source)
+      @source = source
+      @current = 0
+    end
+
+    def next
+      return nil if @current >= source.length
+      token = []
+      values = []
+      source[current..-1].chars.each_with_index do |c, i|
+        # puts "c: #{c} i: #{i}"
+        if c == ' ' || i == source.length-1
+          if token.size > 0
+            @current = current+i+1
+            return token.join
+          end
+        else
+          token << c
+        end
+      end
+      @current = source.length
+
+      token.join
+    end
+  end
+
+  class Parser
+    attr_reader :tokenizer
+
+    def initialize(tokenizer)
+      @tokenizer = tokenizer
+    end
+
+    def parse
+      result = []
+      prev_token = nil
+      while token = tokenizer.next do
+        # puts "token: #{token}"
+        token = token.downcase
+
+        case token
+        when SELECT, FROM, WHERE, HAVING, WINDOW, UNION,
+             INTERSECT, EXCEPT, LIMIT, OFFSET, FETCH, FOR
+          result << [token]
+        else
+          if prev_token == GROUP && token == BY ||
+             prev_token == ORDER && token == BY
+
+            # Remove 'group', 'order' in the last element
+            result.last.pop
+
+            result << ["#{prev_token} #{token}"]
+          else
+            result.last << token
+          end
+        end
+
+        prev_token = token
+      end
+      result
+    end
+
+  end
+
+  def kaigyo
+    return self unless self.include?("select") or self.include?("SELECT")
+
+    tokenizer = Tokenizer.new(self)
+    parser = Parser.new(tokenizer)
+    tree = parser.parse
+    tree.map do |node|
+      "#{node.first} #{node[1..].join(' ')}"
+    end.join("\n")
+  end
 end
 
 class String
--- test/kaigyo_test.rb
+++ test/kaigyo_test.rb
@@ -5,7 +5,23 @@ class KaigyoTest < Minitest::Test
     refute_nil ::Kaigyo::VERSION
   end
 
-  def test_kaigyo
-    assert_equal "kaigyo", ''.kaigyo
+  def test_string_not_sql
+    assert_equal "invalid sql", "invalid sql".kaigyo
+  end
+
+  def test_only_select
+    result = "select a,b".kaigyo
+
+    assert_equal "select a,b", result
+  end
+
+  def test_select_and_from
+    result = "select a,b from foo".kaigyo
+    assert_equal "select a,b\nfrom foo" , result
+  end
+
+  def test_more_complicated_query
+    result = "select a,b from foo inner join bar on foo.id = bar.id where a = 1 group by c order by d".kaigyo
+    assert_equal "select a,b\nfrom foo inner join bar on foo.id = bar.id\nwhere a = 1\ngroup by c\norder by d", result
   end
 end
