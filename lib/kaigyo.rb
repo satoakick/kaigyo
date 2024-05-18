90425d9f1a815391b980e933bd94d9036434ebd4
satoakick <satoakick@gmail.com>
2024-05-18 11:54:34 +0900

    Remove Parser class and Indent `join` node

--- lib/kaigyo.rb
+++ lib/kaigyo.rb
@@ -5,61 +5,6 @@ require "kaigyo/parser"
 module Kaigyo
   class Error < StandardError; end
 
-  WITH = 'with'
-  SELECT = 'select'
-  FROM = 'from'
-  WHERE = 'where'
-  GROUP = 'group'
-  HAVING = 'having'
-  WINDOW = 'window'
-  UNION = 'union'
-  INTERSECT = 'intersect'
-  EXCEPT = 'except'
-  ORDER = 'order'
-  LIMIT = 'limit'
-  OFFSET = 'offset'
-  FETCH = 'fetch'
-  FOR = 'for'
-  BY = 'by'
-  LEFT_PAREN = '('
-  RIGHT_PAREN = ')'
-
-  class Parser
-    attr_reader :tokenizer
-
-    def initialize(tokenizer)
-      @tokenizer = tokenizer
-    end
-
-    def parse
-      result = []
-      prev_token = nil
-      while token = tokenizer.next do
-        # puts "token: #{token}"
-
-        case token.downcase
-        when SELECT, FROM, WHERE, HAVING, WINDOW, UNION,
-             INTERSECT, EXCEPT, LIMIT, OFFSET, FETCH, FOR
-          result << [token]
-        else
-          if prev_token&.downcase == GROUP && token.downcase == BY ||
-             prev_token&.downcase == ORDER && token.downcase == BY
-
-            # Remove 'group', 'order' in the last element
-            result.last.pop
-
-            result << ["#{prev_token} #{token}"]
-          else
-            result.last << token
-          end
-        end
-
-        prev_token = token
-      end
-      result
-    end
-  end
-
   def kaigyo
     return self unless self.downcase.include?("select")
 
@@ -67,7 +12,11 @@ module Kaigyo
     parser = Parser.new(tokenizer)
     tree = parser.parse
     tree.map do |node|
-      "#{node.first} #{node[1..].join(' ')}"
+      str = "#{node.first} #{node[1..].join(' ')}"
+      if node.first.downcase.include? 'join'
+        str = "  " + str
+      end
+      str
     end.join("\n")
   end
 end
