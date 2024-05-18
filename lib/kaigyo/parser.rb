dc26767b4c19942abd038f05ebfb382ef55d1a59
satoakick <satoakick@gmail.com>
2024-05-18 11:53:01 +0900

    Add `Parser` class into `Kaigyo` module

--- lib/kaigyo/parser.rb
+++ lib/kaigyo/parser.rb
@@ -1,55 +1,85 @@
-class Parser
-  WITH = 'with'
-  SELECT = 'select'
-  FROM = 'from'
-  WHERE = 'where'
-  GROUP = 'group'
-  HAVING = 'having'
-  WINDOW = 'window'
-  UNION = 'union'
-  INTERSECT = 'intersect'
-  EXCEPT = 'except'
-  ORDER = 'order'
-  LIMIT = 'limit'
-  OFFSET = 'offset'
-  FETCH = 'fetch'
-  FOR = 'for'
-  BY = 'by'
-  LEFT_PAREN = '('
-  RIGHT_PAREN = ')'
+module Kaigyo
+  class Parser
+    WITH = 'with'
+    SELECT = 'select'
+    FROM = 'from'
+    WHERE = 'where'
+    GROUP = 'group'
+    HAVING = 'having'
+    WINDOW = 'window'
+    UNION = 'union'
+    INTERSECT = 'intersect'
+    EXCEPT = 'except'
+    ORDER = 'order'
+    LIMIT = 'limit'
+    OFFSET = 'offset'
+    FETCH = 'fetch'
+    FOR = 'for'
+    BY = 'by'
+    LEFT_PAREN = '('
+    RIGHT_PAREN = ')'
+    INNER = 'inner'
+    OUTER = 'outer'
+    LEFT = 'left'
+    RIGHT = 'right'
+    FULL = 'full'
+    CROSS = 'cross'
+    JOIN = 'join'
 
-  attr_reader :tokenizer
+    attr_reader :tokenizer
 
-  def initialize(tokenizer)
-    @tokenizer = tokenizer
-  end
-
-  def parse
-    result = []
-    prev_token = nil
-    while token = tokenizer.next do
-      # puts "token: #{token}"
-
-      case token.downcase
-      when SELECT, FROM, WHERE, HAVING, WINDOW, UNION,
-           INTERSECT, EXCEPT, LIMIT, OFFSET, FETCH, FOR
-        result << [token]
-      else
-        if prev_token&.downcase == GROUP && token.downcase == BY ||
-           prev_token&.downcase == ORDER && token.downcase == BY
-
-          # Remove 'group', 'order' in the last element
-          result.last.pop
-
-          result << ["#{prev_token} #{token}"]
-        else
-          result.last << token
-        end
-      end
-
-      prev_token = token
+    def initialize(tokenizer)
+      @tokenizer = tokenizer
     end
-    result
+
+    def parse
+      result = []
+      prev_token = nil
+      prev_prev_token = nil
+
+      while token = tokenizer.next do
+        # puts "token: #{token}"
+
+        prev_prev_token_downcase = prev_prev_token&.downcase
+        prev_token_downcase = prev_token&.downcase
+        token_downcase = token.downcase
+
+        case token_downcase
+        when SELECT, FROM, WHERE, HAVING, WINDOW, UNION,
+             INTERSECT, EXCEPT, LIMIT, OFFSET, FETCH, FOR
+          result << [token]
+        else
+          if prev_token_downcase == GROUP && token_downcase == BY ||
+             prev_token_downcase == ORDER && token_downcase == BY
+
+            # Remove 'group', 'order' in the last element
+            result.last.pop
+
+            result << ["#{prev_token} #{token}"]
+          elsif token_downcase == JOIN
+            case prev_token_downcase
+            when INNER, LEFT, RIGHT, FULL, CROSS
+              result.last.pop
+              result << ["#{prev_token} #{token}"]
+            when OUTER
+              case prev_prev_token_downcase
+              when LEFT, RIGHT, FULL
+                result.last.pop
+                result.last.pop
+                result << ["#{prev_prev_token} #{prev_token} #{token}"]
+              end
+            else
+              result << [token]
+            end
+          else
+            result.last << token
+          end
+        end
+
+        prev_prev_token = prev_token
+        prev_token = token
+      end
+      result
+    end
   end
 end
-
