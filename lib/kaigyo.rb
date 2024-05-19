12f3286afbec3b5f6d416f0758f5e25ce2429ddc
satoakick <satoakick@gmail.com>
2024-05-20 05:21:44 +0900

    Fix indent and formatting

--- lib/kaigyo.rb
+++ lib/kaigyo.rb
@@ -10,19 +10,35 @@ module Kaigyo
     tokenizer = Tokenizer.new(self)
     tokens = tokenizer.token_analysis
     result = []
+    prev_token = nil
+    indent_size = 0
+
     tokens.each do |token|
+      if token.first == :left_paren
+        indent_size += 1
+      end
+      if token.first == :right_paren
+        indent_size -= 1
+      end
+
       if token.first == :clause
-        result << [token[1]]
+        result << [indent(indent_size) + token[1]]
       elsif token.first == :and || token.first == :or
-        result << ['  ' + token[1]]
+        result << [indent(indent_size) + '  ' + token[1]]
       else
         result.last << token[1]
       end
+
+      prev_token = token
     end
 
     result.map do |row|
-      row.join(' ')
+      row.inject("") { |str, c| str += c == ',' ? c : " #{c}" }.strip
     end.join("\n")
+  end
+
+  def indent(size)
+    '  ' * size
   end
 end
 
--- test/kaigyo_test.rb
+++ test/kaigyo_test.rb
@@ -10,18 +10,18 @@ class KaigyoTest < Minitest::Test
   end
 
   def test_only_select
-    result = "select a,b".kaigyo
+    result = "select a, b".kaigyo
 
-    assert_equal "select a,b", result
+    assert_equal "select a, b", result
   end
 
   def test_select_and_from
-    result = "select a,b from foo".kaigyo
-    assert_equal "select a,b\nfrom foo" , result
+    result = "select a, b from foo".kaigyo
+    assert_equal "select a, b\nfrom foo" , result
   end
 
   def test_more_complicated_query
-    result = "select a,b from foo inner join bar on foo.id = bar.id where a = 1 group by c order by d".kaigyo
-    assert_equal "select a,b\nfrom foo\n  inner join bar on foo.id = bar.id\nwhere a = 1\ngroup by c\norder by d", result
+    result = "select a, b from foo inner join bar on foo.id = bar.id where a = 1 group by c order by d".kaigyo
+    assert_equal "select a, b\nfrom foo\ninner join bar on foo.id = bar.id\nwhere a = 1\ngroup by c\norder by d", result
   end
 end
